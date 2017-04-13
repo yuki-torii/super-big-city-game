41fb11eefe2fae4ee6b328431f05eefd1911f0d4
limichange <limichange@hotmail.com>
2017-04-13 23:33:43 +0800

    Update

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
 import 'normalize.css'
 
+Vue.use(ElementUI)
 Vue.config.productionTip = true
 
 /* eslint-disable no-new */
--- src/router/indexPage.vue
+++ src/router/indexPage.vue
@@ -1,7 +1,32 @@
 <template>
   <div class="indexPage">
-    <h2>状态栏</h2>
-    <div>第 {{ time }} 天</div>
+    <div v-if="stage === 0">
+      <div>这是一片空地，你将建立起自己的城市</div>
+      <el-button @click="stage = 1">嗯，好的</el-button>
+    </div>
+    <div v-if="stage === 1">
+      <el-row>
+        <el-col :span="8">
+          <p class="tips">你现在需要收集木材升起篝火 🔥</p>
+          <p>统计</p>
+          <el-button>人口 {{ people }}</el-button>
+          <el-button>木材 {{ wood }}</el-button>
+          <p>包裹</p>
+          <el-button v-for="item in bag">
+            {{ item.name }}
+          </el-button>
+        </el-col>
+        <el-col :span="8">
+          <p>采集</p>
+          <el-button @click="wood++">收集木材</el-button>
+          <p>生产</p>
+          <el-button @click="bag.push({ name: '篝火' }); wood -= 10">篝火</el-button>
+          <p>建造</p>
+          <el-button>帐篷</el-button>
+        </el-col>
+        <el-col :span="8"><div class="">3</div></el-col>
+      </el-row>
+    </div>
   </div>
 </template>
 
@@ -10,17 +35,18 @@ export default {
   name: 'indexPage',
   data () {
     return {
+      people: 1,
       time: 0,
       wood: 0,
-      forest: 1000
+      stage: 0,
+      bag: []
     }
+  },
+  watch: {
   },
   mounted () {
     const self = this
-    setInterval(() => {
-      self.time++
-      self.forest--
-    }, 1000)
+    console.log(self)
   }
 }
 </script>
